6af76a2d87f0d4237317b49714d30926dc0ec42a
mkizka <g.mkizka@gmail.com>
2024-04-20 18:37:35 +0900

    tsxに移行

--- src/cron.ts
+++ src/cron.ts
@@ -1,12 +1,22 @@
 import { CronJob } from 'cron'
 
+import { env } from './util/env'
+
+const cronTimes = {
+  // 10秒に1回実行
+  development: '*/10 * * * * *',
+  // 毎日午前0時10分に実行
+  production: '0 10 0 * * *',
+  // productionと同じ
+  test: '0 10 0 * * *',
+} satisfies Record<typeof env.NODE_ENV, string>
+
 export const startCron = () => {
   const job = CronJob.from({
-    cronTime: '0 * * * * *',
+    cronTime: cronTimes[env.NODE_ENV],
     onTick: () => {
       console.log('CRON実装テスト', new Date())
     },
-    start: true,
     timeZone: 'Asia/Tokyo',
   })
   job.start()
--- src/util/env.ts
+++ src/util/env.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv'
 import { z } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   TMDB_AUTH_TOKEN: z.string(),
 })
 
@@ -9,6 +11,7 @@ export const env = (() => {
   if (process.env.NODE_ENV === 'test') {
     return process.env as z.infer<typeof envSchema>
   }
+  dotenv.config()
   try {
     return envSchema.parse(process.env)
   } catch (err) {
